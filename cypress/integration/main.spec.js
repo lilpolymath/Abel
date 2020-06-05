2f1e3a1f32a97f70ec908d238b66837e8bf502c5
lilpolymath <ayofavour7@gmail.com>
2020-06-05 19:50:14 +0100

    chore: working on the homepage tests.

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -32,15 +32,21 @@ describe('Homepage', () => {
   });
 
   it('should have - two sign-in button', () => {
-    cy.get('a').should('contain', 'Sign in');
+    cy.get('a')
+      .contains('Sign in')
+      .should('have.attr', 'href', '/sign-in');
   });
 
   it('should have - two create account button', () => {
-    cy.get('a').should('contain', 'Create an account');
+    cy.get('a')
+      .contains('Create an account')
+      .should('have.attr', 'href', '/create-an-account');
   });
 
   it('should have - forgot password button', () => {
-    cy.get('a').should('contain', 'Forgot Password?');
+    cy.get('a')
+      .contains('Forgot Password?')
+      .should('have.attr', 'href', '/forgot-password');
   });
 });
 
